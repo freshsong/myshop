7c04e81952497ad698136d46743b4cd75a515b04
신선한 노래 <racimo.715@gmail.com>
2024-02-06 11:15:02 +0900

    2월6일추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-$(function(){
+$(function(){    
+
     $(document).on("mouseenter",'.pr-category>li',function(){
         $(this).find('.sub-cate').fadeIn();
     })
@@ -11,6 +12,25 @@ $(function(){
     }).mouseleave(function(){
         $(this).find('.category-subnav').css('display', 'none');
     });
+    $(window).scroll(function(){
+        let tops = $(window).scrollTop();  
+        if(tops > 0) {
+           $('header').css({
+              'position': 'fixed',
+              'backgroundColor': 'rgba(255,255,255,0.8)',
+              'top': 0,
+              'width': '100%',
+              'zIndex':1000
+           }).addClass('fixed');
+        }else{
+          $('header').css({
+             'position': 'static',
+             'backgroundColor': 'rgba(255,255,255)',
+             'top': 0,
+             'width': '100%'
+          }).removeClass('fixed');        
+        } 
+     });
     
     $('.listview').click(function(e){ //event의 e
         e.preventDefault();
@@ -29,4 +49,52 @@ $(function(){
             $("#pdlist>div").addClass("col-md-55 mb-5")
         }
     });
+
+    $('.tops').click(function(e){
+        e.preventDefault(); //기능중지
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    $('.news').click(function(e){
+        e.preventDefault();
+        let thenew;
+        if($('header').css('position','fixed')){
+            thenew = $('#new').offset().top - 100;
+        }else{
+            thenew = $('#new').offset().top - 200;
+        }
+        $('html, body').animate({
+            scrollTop: thenew + "px"
+        })
+    });
+
+    $('.hots').click(function(e){
+        e.preventDefault();
+        let thenew;
+        if($('header').css('position','fixed')){
+            thenew = $('#list').offset().top - 100;
+        }else{
+            thenew = $('#list').offset().top - 200;
+        }
+        $('html, body').animate({
+            scrollTop: thenew + "px"
+        })
+    });
+
+    $('.hots').click(function(e){
+        e.preventDefault();
+        
+    });
+
+    $('.bottoms').click(function(e){
+        e.preventDefault(); //
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 500);
+    });
+
+
 });
+
