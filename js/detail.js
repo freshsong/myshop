4a60672385674c40a2e160871850ee9493fca0aa
신선한 노래 <racimo.715@gmail.com>
2024-02-06 13:19:34 +0900

    2월 6일 오전수업 끝

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,12 @@
 $(function(){
     $('.img-thumb-box>img').click(function(){
         const src = $(this).attr('src');
-        $('.img-box>img').attr('src', src);
+        $('.img-box>img').attr('src', src);  
     });
+    $('.colors input[type=radio]').click(function(){
+        const color = $(this).val();
+        //alert(color);
+        $('.selected').text(color+'색');
+    });   
+
 });
